6c368e3f490b65ffe2f8e70d13ef44af216e79b1
Edison Waweru <wambuguedison@gmail.com>
2021-01-05 10:38:22 +0300

    optimized build

---
+++ precache-manifest.2cab61a0fcebea82bb8af1760d06b824.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8957e95dbdd777fac043afd065e7cf85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c72b42e98ee42f76db2",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "98194d3b1c118d5ea792",
+    "url": "/static/css/main.347e5c9f.chunk.css"
+  },
+  {
+    "revision": "9c72b42e98ee42f76db2",
+    "url": "/static/js/2.7f1d5fb2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7f1d5fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98194d3b1c118d5ea792",
+    "url": "/static/js/main.cd8e1d44.chunk.js"
+  },
+  {
+    "revision": "c5a16b320f74bc52a28b",
+    "url": "/static/js/runtime-main.0d386bcb.js"
+  },
+  {
+    "revision": "bea35746fb7cdd67b15ecf0622d8cc85",
+    "url": "/static/media/Kalam-Regular.bea35746.ttf"
+  }
+]);
---
+++ precache-manifest.cf5d97ae8b45462183f34cb970c5c5a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cdfd0107dd83937c10ab0ca4de37dfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c72b42e98ee42f76db2",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d7cfea8adb7f07de9e08",
+    "url": "/static/css/main.d82dc6dd.chunk.css"
+  },
+  {
+    "revision": "9c72b42e98ee42f76db2",
+    "url": "/static/js/2.7f1d5fb2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7f1d5fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7cfea8adb7f07de9e08",
+    "url": "/static/js/main.dbcc2359.chunk.js"
+  },
+  {
+    "revision": "c5a16b320f74bc52a28b",
+    "url": "/static/js/runtime-main.0d386bcb.js"
+  },
+  {
+    "revision": "bea35746fb7cdd67b15ecf0622d8cc85",
+    "url": "/static/media/Kalam-Regular.bea35746.ttf"
+  }
+]);
